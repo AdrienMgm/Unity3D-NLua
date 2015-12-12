5711e11b01ad948967772ab6e2121547e87c8579
Jose Castorena <josec@brasquestudios.com>
2015-12-12 07:56:28 -0800

    Adding support for mobile platforms

---
+++ Assets/Example/NLuaBehaviour.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using NLua;
+
+public abstract class NLuaBehaviour : MonoBehaviour
+{
+    protected bool initialized;
+    protected Lua env;
+
+    protected abstract string LuaScriptFileName
+    {
+        get;
+    }
+
+    public virtual void Awake()
+    {
+        env = new Lua();
+        env.LoadCLRPackage();
+
+        StartCoroutine(LoadFile(LuaScriptFileName));
+    }
+
+    public System.Object[] Call(string function, params System.Object[] args)
+    {
+        System.Object[] result = new System.Object[0];
+        if (env == null) return result;
+        LuaFunction lf = env.GetFunction(function);
+        if (lf == null) return result;
+        try
+        {
+            // Note: calling a function that does not
+            // exist does not throw an exception.
+            if (args != null)
+            {
+                result = lf.Call(args);
+            }
+            else
+            {
+                result = lf.Call();
+            }
+        }
+        catch (NLua.Exceptions.LuaException e)
+        {
+            Debug.LogError(FormatException(e), gameObject);
+        }
+        return result;
+    }
+
+    public System.Object[] Call(string function)
+    {
+        return Call(function, null);
+    }
+
+    public static string FormatException(NLua.Exceptions.LuaException e)
+    {
+        string source = (string.IsNullOrEmpty(e.Source)) ? "<no source>" : e.Source.Substring(0, e.Source.Length - 2);
+        return string.Format("{0}\nLua (at {2})", e.Message, string.Empty, source);
+    }
+
+    public IEnumerator LoadFile(string luaFileName)
+    {
+        string filePath = Application.streamingAssetsPath + "/" + luaFileName;
+
+        string luaScript = null;
+        if (filePath.Contains("://"))
+        {
+            WWW www = new WWW(filePath);
+            yield return www;
+
+            luaScript = www.text;
+        }
+        else
+        {
+            luaScript = System.IO.File.ReadAllText(filePath);
+        }
+
+        try
+        {
+            env.DoString(luaScript);
+        }
+        catch (NLua.Exceptions.LuaException e)
+        {
+            Debug.LogError(FormatException(e), gameObject);
+        }
+
+        initialized = true;
+    }
+}
--- README.md
+++ README.md
@@ -42,4 +42,18 @@ See the SpawnSphere example.
 
 **How do I run C# coroutines?**
 
-See [this comment](https://github.com/NLua/NLua/issues/110#issuecomment-59874806) for details, essentially though you either have to call lua functions indriectily, or roll your own coroutine manager (not hugely difficult). Direct support for coroutines may be included in future releases. 
+See [this comment](https://github.com/NLua/NLua/issues/110#issuecomment-59874806) for details, essentially though you either have to call Lua functions indirectly, or roll your own coroutine manager (not hugely difficult). Direct support for coroutines may be included in future releases. 
+
+**How do I build for iOS?**
+
+iOS is more difficult to build for because the code is compiled using AOT compilation and Unity strips all assemblies by default. To get around this, there are a few steps you must take:
+
+- Make sure you use the NLua.dll inside of the iOS folder.
+- Prevent stripping of your code. Unity strips your code by looking at all of the references inside of your assemblies. Because Lua scripts are not part of compilation process, anything not referenced inside of your C# code or plugins will be stripped.
+To prevent this, make sure any Unity or .dll code is used inside your C# code or add any namespaces or assemblies to the link.xml file. See [this page] (http://docs.unity3d.com/Manual/iphone-iOS-Optimization.html) for more details.
+- Make sure you have your compatibility mode set to .net 2.0 subset. There is a bug with running NLua on iOS currently that is fixed by having compatibility set to .net 2.0 subset. See [this page] (http://forum.unity3d.com/threads/unity-5-0-3f2-il2cpp-problem-attempting-to-call-method-system-reflection-monoproperty-getteradapt.332335/) for more details.
+
+**How do I rebuild NLua.dll?**
+
+There are two .csproj files inside of the /src/ folders: one for windows store applications (NLua_WSA.csproj) and another for all other platforms (NLua.csproj). Simple open them up with Visual Studio, select your configuration, and rebuild.
+Alternatively, you can remove the NLua.dll files from the plugins folder and use the scripts from the src folder directly. iOS has problems running NLua this way, however.
